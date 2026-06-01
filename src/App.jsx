d5be88770830744ffe876764a737d50c98449695
FarmaJava <facundomaldonado258@gmail.com>
2026-06-01 16:14:23 -0300

    commit 1.0

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="center">
+      </section>
+    </>
+  )
+}
+
+export default App
---
+++ src/firebase/config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBTihwyMyHz_BODY33l3tfsnpdjxwF0pYQ",
+  authDomain: "copa-renualt-app.firebaseapp.com",
+  projectId: "copa-renualt-app",
+  storageBucket: "copa-renualt-app.firebasestorage.app",
+  messagingSenderId: "640305985431",
+  appId: "1:640305985431:web:f9eec6883bae45c31bf6d2",
+  measurementId: "G-FR0NWDTV6M"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
